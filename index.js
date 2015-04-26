7f3559ed3fbcab047655871c70ca2a2ff7a78011
Zhang Shuai <satan.student@gmail.com>
2015-04-26 18:08:49 +0800

    Encode {}.

--- index.js
+++ index.js
@@ -5,19 +5,24 @@ var cheerio = require('cheerio');
 var asciidoctor = require('asciidoctor.js')();
 var processor = asciidoctor.Asciidoctor(true);
 
+var options = {
+  lang: 'plain',
+  gutter: false,
+  wrap: false
+};
+
 hexo.extend.renderer.register('adoc', 'html', function(data, locals) {
   var html = processor.$convert(data.text, null);
   var $ = cheerio.load(html);
 
   $('.highlight code').each(function(index, elem) {
-    var result = util.highlight($(elem).text(), {
-      lang: elem.attribs['lang-data'],
-      gutter: false,
-      wrap: false
-    });
-    $(elem).html(result);
+    options.lang = elem.attribs['lang-data'];
+    var result = util.highlight($(elem).text(), options);
+    $(elem).html('<escape>' + result + '</escape>');
   });
 
-  return $.html();
+  return $.html()
+             .replace(/{/g, '&#123;')
+             .replace(/}/g, '&#125;');
 }, true);
 
