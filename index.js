65792025b6f80d121e46766b2d9c283f64ad49a6
Zhang Shuai <satan.student@gmail.com>
2015-04-26 17:34:16 +0800

    Use cheerio and hexo-util.highlight to do the static syntax highlight.

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 'use strict'
 
+var util = require('hexo-util');
+var cheerio = require('cheerio');
 var asciidoctor = require('asciidoctor.js')();
 var processor = asciidoctor.Asciidoctor(true);
 
 hexo.extend.renderer.register('adoc', 'html', function(data, locals) {
-  return processor.$convert(data.text, null);
+  var html = processor.$convert(data.text, null);
+  var $ = cheerio.load(html);
+
+  $('.highlight code').each(function(index, elem) {
+    var result = util.highlight($(elem).text(), {
+      lang: elem.attribs['lang-data'],
+      gutter: false,
+      wrap: false
+    });
+    $(elem).html(result);
+  });
+
+  return $.html();
 }, true);
 
