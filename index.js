3ab30e7143a3d71004242e89a68b20901e70357d
Zhang Shuai <satan.student@gmail.com>
2015-04-27 23:42:55 +0800

    Bugfix, use data-lang instead of lang-data.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var asciidoctor = require('asciidoctor.js')();
 var processor = asciidoctor.Asciidoctor(true);
 
 var options = {
+  auto_detect: false,
   lang: 'plain',
   gutter: false,
   wrap: false
@@ -16,9 +17,9 @@ hexo.extend.renderer.register('adoc', 'html', function(data, locals) {
   var $ = cheerio.load(html);
 
   $('.highlight code').each(function(index, elem) {
-    options.lang = elem.attribs['lang-data'];
-    var result = util.highlight($(elem).text(), options);
-    $(elem).html(result);
+    options.lang = elem.attribs['data-lang'];
+    var content = util.highlight($(elem).text(), options);
+    $(elem).html(content);
   });
 
   return $.html()
