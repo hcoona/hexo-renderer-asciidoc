ec3649da35fedde0bcce86d583612f5556652de4
Shuai Zhang <shuaiz@microsoft.com>
2017-03-06 16:07:44 +0800

    Rollback to earlier version of asciidoctor.js; Add testing & src lint etc.

--- index.js
+++ index.js
@@ -1,40 +1,8 @@
-'use strict'
+/* global hexo */
 
-var entities = require('entities');
-var util = require('hexo-util');
-var cheerio = require('cheerio');
-var asciidoctor = require('asciidoctor.js')();
+'use strict';
 
-var options = {
-  autoDetect: false,
-  lang: 'plain',
-  gutter: false,
-  wrap: false
-};
-
-var cheerio_load_option = {
-  decodeEntities: false
-};
-
-var asciidoc_options = {
-  doctype: 'book'
-};
-
-function renderer(data, locals) {
-  var html = asciidoctor.convert(data.text);
-  var $ = cheerio.load(html, cheerio_load_option);
-
-  $('.highlight code').each(function(index, elem) {
-    options.lang = elem.attribs['data-lang'];
-    var code = entities.decodeXML($(elem).text());
-    var content = util.highlight(code, options);
-    $(elem).html(content);
-  });
-
-  return $.html()
-             .replace(/{/g, '&#123;')
-             .replace(/}/g, '&#125;');
-}
+var renderer = require('./lib/renderer');
 
 hexo.extend.renderer.register('ad', 'html', renderer, true);
 hexo.extend.renderer.register('adoc', 'html', renderer, true);
---
+++ test/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var should = require('chai').should(); // eslint-disable-line
+
+describe('Asciidoc renderer', function() {
+  var r = require('../lib/renderer');
+
+  it('header', function() {
+    var body = `
+== Test H2 ==
+`;
+    var result = r({text: body}, {});
+
+    result.should.eql(`<div class="sect1">
+<h2 id="_test_h2">Test H2</h2>
+<div class="sectionbody">
+
+</div>
+</div>`);
+  });
+
+  it('code highlight', function() {
+    var body = `
+[source,ruby]
+----
+require 'sinatra'
+
+get '/hi' do
+  "Hello World!"
+end
+----`;
+    var result = r({text: body}, {});
+
+    result.should.eql(`<div class="listingblock">
+<div class="content">
+<pre class="highlight"><code class="language-ruby" data-lang="ruby"><span class="keyword">require</span> <span class="string">'sinatra'</span>
+
+get <span class="string">'/hi'</span> <span class="keyword">do</span>
+  <span class="string">"Hello World!"</span>
+<span class="keyword">end</span></code></pre>
+</div>
+</div>`);
+  });
+});
