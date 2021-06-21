c7decc01b4dd9e7a0f94175fdad07e37eb52a597
Shuai Zhang <zhangshuai.ustc@gmail.com>
2021-06-21 20:05:03 +0800

    Upgrade dependencies versions.

--- test/index.js
+++ test/index.js
@@ -1,15 +1,16 @@
 'use strict';
 
+const entities = require("entities");
 var should = require('chai').should(); // eslint-disable-line
 
-describe('Asciidoc renderer', function() {
+describe('Asciidoc renderer', function () {
   var r = require('../lib/renderer');
 
-  it('header', function() {
+  it('header', function () {
     var body = `
 == Test H2 ==
 `;
-    var result = r({text: body}, {});
+    var result = r({ text: body }, {});
 
     result.should.eql(`<div class="sect1">
 <h2 id="_test_h2">Test H2</h2>
@@ -19,7 +20,7 @@ describe('Asciidoc renderer', function() {
 </div>`);
   });
 
-  it('code highlight', function() {
+  it('code highlight', function () {
     var body = `
 [source,ruby]
 ----
@@ -29,9 +30,9 @@ get '/hi' do
   "Hello World!"
 end
 ----`;
-    var result = r({text: body}, {});
+    var result = r({ text: body }, {});
 
-    result.should.eql(`<div class="listingblock">
+    entities.decodeHTML(result).should.eql(entities.decodeHTML(`<div class="listingblock">
 <div class="content">
 <pre class="highlight"><code class="language-ruby" data-lang="ruby"><pre><code class="highlight ruby"><span class="keyword">require</span> <span class="string">'sinatra'</span>
 
@@ -39,6 +40,6 @@ get <span class="string">'/hi'</span> <span class="keyword">do</span>
   <span class="string">"Hello World!"</span>
 <span class="keyword">end</span></code></pre></code></pre>
 </div>
-</div>`);
+</div>`));
   });
 });
